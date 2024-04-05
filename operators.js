469c633c1d50da820e320e9f4481d5444926ad5d
venugopalLebakula <135225101+venugopalLebakula@users.noreply.github.com>
2024-04-05 16:18:18 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,104 @@
+//greater than and less than operators
+EX: 1
+/*
+let a="JAVA";
+let b="SCRIPT";
+let c=a>b;
+console.log(c);//
+Ex:2
+let me=Number('venu');
+let you="gopal";
+let us=me<=you;//comparision 
+console.log(us);
+EX:3
+let p=55;
+let q=65;
+let r=typeof(p)==typeof(q);//== comparsion operator
+console.log(r);// true
+EX:4
+let s="venugopal";
+let t=Number('venugopal');
+let u=typeof(s)!=typeof(t);//!= operator
+console.log(u);
+EX:5
+let i=66;
+let j=Number("venu");
+let k=typeof(i)===typeof j;
+console.log(k);
+EX:6 //ternary opearator
+let v=65;
+let w="venu";
+//let check= "" ? v:w;
+let check1=Number?v:w;
+console.log(check1);
+//condition operators
+//if elseif condition
+*/
+/*
+let Time=9.15;
+if(Time==10.00){
+    console.log("i can go for a movie named:Hanuman" );
+}
+else if(Time>=9&&Time<9.30){
+    console.log("need to book a ticket for movie");
+}
+else{
+    console.log("shut all the doors and turn off the lights and sleep");
+}
+*/
+let aboutMyfriend="gopaiah";
+switch(aboutMyfriend){
+    case "jayakumar":
+        if(aboutMyfriend=="jayakumar"){
+            console.log("Jayakumar is one of my childhood friend and a verygood friend of mine,trustworthy and very helpfull in every situation.very cool and calm");
+        }
+        
+        break;
+        case "gopaiah":
+            if(aboutMyfriend=="gopaiah"){
+                console.log("Very inncent guy with pure heart,He is like a gem of a person having very good character.")
+            }
+           break;
+           case "aravindh":
+           if(aboutMyfriend=="aravindh"){
+            console.log("very lovable and at the samw time toxic in nature,helpfulll and self dabba fellow.once upon a time he is innocent but noe he is not like that,whateverit is he is one of my frined to remember");
+           }
+           break;
+           case "ramgopal":
+            if(aboutMyfriend=="ramgopal"){
+                console.log("He is one of villain in my life with a toxic behaviour which is not to have in friendship.he is selfish and very cunning behaviour. i dont like him as a friend but as a known person i will consider");
+            }
+            break;
+            case "venna":
+                if(aboutMyfriend=="venna"){
+                    console.log("he is a good guy with pure heart in childhood but as of now he was become a attitude guy with some toxic behaviour .but kind hearted person with lot of responsibilities on his shoulder.but a good friend of mine and will be my my frirend");
+                }
+                break;
+            case "Ashok":
+                if(aboutMyfriend=="Ashok"){
+                    console.log("He is short guy with very heigh attitude,he is not a innocent but very funny guy,and cunning person also ,he is a good friend of mine once upon a time.")
+                }
+                break;
+                case "Mani":
+                    if(aboutMyfriend=="mani"){
+                        console.log("he is a very good guy with no toxic behaviour always do funny things and a kind hearted person with lot of friendship values.very helpful and Honesty guy and will be my friend as long as i live" );
+                    }  
+                    break;
+                    case "rakesh":
+                        if(aboutMyfriend=="rakesh"){
+                            console.log("He is talented guy with funny behaviour and always make fun of every thing.but having a kind heart and a trustworthy for everyone with zero attitude.A good friend to have in my life");
+
+                        }  
+                        break;
+                        case "ajayvarma":
+                            if(aboutMyfriend=="ajayvarma"){
+                                console.log("vry talented guy with zero arrogance and agood hearted guy .very helpfull but some time selfish");
+                            }
+                            break;
+                            default:{
+                                console.log("Having friends is ok but having best friends is fine,but having a brother as friend is something special ");
+                            }
+       
+
+        
+}
